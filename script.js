07d36307129faafff4b400b15cf0a6139394c1da
angoladoug <87103931+miserydouglas@users.noreply.github.com>
2021-11-24 21:30:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+
+    let contador = 0
+    document.getElementById("buton").onclick = function(){
+    contador ++
+    document.getElementById("mostrar").innerHTML = contador    
+
+}
+}
